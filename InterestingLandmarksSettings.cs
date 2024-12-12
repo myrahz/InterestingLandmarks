9c573655574acdebecae45ec6f1aba2086774982
SquirrelRat <37273964+SquirrelRat@users.noreply.github.com>
2024-12-11 20:34:58 -0600

    Change Settings Formatting
    To make it easier to navigate

--- InterestingLandmarksSettings.cs
+++ InterestingLandmarksSettings.cs
@@ -9,60 +9,55 @@ namespace InterestingLandmarks
     {
         public ToggleNode Enable { get; set; } = new ToggleNode(false);
 
-        [Menu("Show chests")]
+        [Menu("Show Chests")]
         public ToggleNode ShowChests { get; set; } = new ToggleNode(true);
 
         [Menu("Chests", 100, CollapsedByDefault = true)]
         public EmptyNode SettingsEmptyChest { get; set; }
 
-        [Menu("White chests", parentIndex = 100)]
+        [Menu("White Chests", parentIndex = 100)]
         public ToggleNode ShowWhiteChests { get; set; } = new ToggleNode(true);
-        [Menu("White chest color", parentIndex = 100)]
-        public ColorNode WhiteChestColor { get; set; } = new ColorNode(Color.White);
-
-        [Menu("Magic chests", parentIndex = 100)]
+        [Menu("Magic Chests", parentIndex = 100)]
         public ToggleNode ShowMagicChests { get; set; } = new ToggleNode(true);
-        [Menu("Magic chest color", parentIndex = 100)]
-        public ColorNode MagicChestColor { get; set; } = new ColorNode(Color.Blue);
-
-        [Menu("Rare chests", parentIndex = 100)]
+        [Menu("Rare Chests", parentIndex = 100)]
         public ToggleNode ShowRareChests { get; set; } = new ToggleNode(true);
-        [Menu("Rare chest color", parentIndex = 100)]
-        public ColorNode RareChestColor { get; set; } = new ColorNode(Color.Yellow);
-
-        [Menu("Unique chests", parentIndex = 100)]
+        [Menu("Unique Chests", parentIndex = 100)]
         public ToggleNode ShowUniqueChests { get; set; } = new ToggleNode(true);
-        [Menu("Unique chest color", parentIndex = 100)]
-        public ColorNode UniqueChestColor { get; set; } = new ColorNode(Color.Orange);
-
-        [Menu("Other (?) chests", parentIndex = 100)]
+        [Menu("Other (?) Chests", parentIndex = 100)]
         public ToggleNode ShowOtherChests { get; set; } = new ToggleNode(true);
-        [Menu("Other chest color", parentIndex = 100)]
+
+        [Menu("White Chest color", parentIndex = 100)]
+        public ColorNode WhiteChestColor { get; set; } = new ColorNode(Color.White);
+        [Menu("Magic Chest color", parentIndex = 100)]
+        public ColorNode MagicChestColor { get; set; } = new ColorNode(Color.Blue);
+        [Menu("Rare Chest Color", parentIndex = 100)]
+        public ColorNode RareChestColor { get; set; } = new ColorNode(Color.Yellow);
+        [Menu("Unique Chest Color", parentIndex = 100)]
+        public ColorNode UniqueChestColor { get; set; } = new ColorNode(Color.Orange);
+        [Menu("Other Chest Color", parentIndex = 100)]
         public ColorNode OtherChestColor { get; set; } = new ColorNode(Color.Gray);
 
-        [Menu("Show area transitions")]
+        [Menu("Show Area Transitions")]
         public ToggleNode ShowTransitions { get; set; } = new ToggleNode(true);
-        [Menu("Area transition color")]
-        public ColorNode TransitionsColor { get; set; } = new ColorNode(Color.White);
-
-        [Menu("Show waypoints")]
+        [Menu("Show Waypoints")]
         public ToggleNode ShowWaypoints { get; set; } = new ToggleNode(true);
-        [Menu("Waypoint color")]
-        public ColorNode WaypointsColor { get; set; } = new ColorNode(Color.LightBlue);
-
-        [Menu("Show points of interest (minimap icons)")]
+        [Menu("Show Points of Interest (Minimap Icons)")]
         public ToggleNode ShowPoI { get; set; } = new ToggleNode(true);
-        [Menu("Point of interest color")]
-        public ColorNode PoIColor { get; set; } = new ColorNode(Color.LightGreen);
-
-        [Menu("Show essences")]
+        [Menu("Show Essences")]
         public ToggleNode ShowEssence { get; set; } = new ToggleNode(true);
-        [Menu("Essence color")]
-        public ColorNode EssenceColor { get; set; } = new ColorNode(Color.Purple);
-		
 	[Menu("Show Switches")]
         public ToggleNode ShowSwitch { get; set; } = new ToggleNode(true);
-        [Menu("Essence color")]
+		
+		
+        [Menu("Area Transition Color")]
+        public ColorNode TransitionsColor { get; set; } = new ColorNode(Color.White);
+        [Menu("Waypoint Color")]
+        public ColorNode WaypointsColor { get; set; } = new ColorNode(Color.LightBlue);
+        [Menu("Point of Interest Color")]
+        public ColorNode PoIColor { get; set; } = new ColorNode(Color.LightGreen);
+        [Menu("Essence Color")]
+        public ColorNode EssenceColor { get; set; } = new ColorNode(Color.Purple);
+        [Menu("Switch Color")]
         public ColorNode SwitchColor { get; set; } = new ColorNode(Color.Red);
     }
 }
